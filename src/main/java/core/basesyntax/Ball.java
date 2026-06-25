7b58ecd056e2614478ad5480521b052371a36b4c
user <pawel.m.janik@gmail.com>
2026-06-25 23:50:34 +0200

    Code formatting

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -1,14 +1,11 @@
 package core.basesyntax;
-
 public class Ball {
     private final String color;
     private final int number;
-
     public Ball(String color, int number) {
         this.color = color;
         this.number = number;
     }
-
     @Override
     public String toString() {
         return color + " " + number;
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
-
 import java.util.Random;
 
 public class Lottery {
     public Ball getRandomBall() {
+        int maxValue = 101;
         ColorSupplier colorSupplier = new ColorSupplier();
         String randomColor = colorSupplier.getRandomColor();
         Random random = new Random();
-        int randomNumber = random.nextInt(101);
+        int randomNumber = random.nextInt(maxValue);
         return new Ball(randomColor, randomNumber);
     }
 }
