800778d2956a3f23f4280ad887ebf081d6004902
user <pawel.m.janik@gmail.com>
2026-06-26 00:08:41 +0200

    In Lottery class: added private and final to Random and ColorSupplier variables. In ColorSupplier class: added private and final to Random and Colors variables.

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    Random random = new Random();
-    Colors[] colors = Colors.values();
+    private final Random random = new Random();
+    private final Colors[] colors = Colors.values();
 
     public String getRandomColor() {
-        int randomIndex = random.nextInt(colors.length);
-        return colors[randomIndex].name();
+        int randomIndex = this.random.nextInt(colors.length);
+        return this.colors[randomIndex].name();
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
-    ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
+    private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Ball getRandomBall() {
         int maxNumber = 100;
-        return new Ball(colorSupplier.getRandomColor(), random.nextInt(maxNumber + 1));
+        return new Ball(this.colorSupplier.getRandomColor(), this.random.nextInt(maxNumber + 1));
     }
 }
